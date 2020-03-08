fa6ff823e8d11325c2e0389d07e2a2e65e77a0f4
Robin Stenkilsson <robinstenkilsson@Robins-MacBook-Air.local>
2020-03-08 10:19:56 +0100

    Footer

--- src/components/molecules/Footer.js
+++ src/components/molecules/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Footer(props) {
   return (
     <footer className="footer">
-      <p>The Movie DB. A React project made by <a href="https://www.linkedin.com/in/robin-stenkilsson">Robin Stenkilsson.</a></p>
+      <div className="page-wrapper">
+        <p>The Movie DB. A React project made by <a href="https://www.linkedin.com/in/robin-stenkilsson">Robin Stenkilsson.</a></p>
+      </div>
     </footer>
   )
 }
